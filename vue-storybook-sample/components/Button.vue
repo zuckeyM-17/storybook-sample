edfd0c1afa12342ba62828bdb56949bb8ab91f91
松村和輝 <k.matsumura0314@gmail.com>
2018-02-21 00:50:57 +0900

    [Vue] add Button.vue

---
+++ vue-storybook-sample/components/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <button @click="handleClick">{{ children }}</button>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["handleClick", "children"],
+});
+</script>
